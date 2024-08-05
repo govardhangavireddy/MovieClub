9bab8d03caaa8a4368615c7f59e4dac351ad2c9e
Sai Govardhan Naidu Gavireddy <sgavired@gitam.in>
2024-08-05 16:07:19 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,30 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  let [watchList,setWatchlist] = useState([])
+  let [watchList, setWatchlist] = useState([]);
 
   let handleAddtoWatchlist = (movie) => {
-    let newWatchList = [...watchList,movie]
-    localStorage.setItem('watchlist',JSON.stringify(newWatchList))
-    setWatchlist(newWatchList)
-    console.log(newWatchList)
-    
-  }
+    let newWatchList = [...watchList, movie];
+    localStorage.setItem("watchlist", JSON.stringify(newWatchList));
+    setWatchlist(newWatchList);
+    console.log(newWatchList);
+  };
 
   let handleRemoveFromWatchlist = (movie) => {
-    let newRemoveWatchlist = watchList.filter((item)=>{
-      return item.id!==movie.id
-    })
-    localStorage.setItem('watchlist',JSON.stringify(newRemoveWatchlist))
-    setWatchlist(newRemoveWatchlist)
-    console.log(newRemoveWatchlist)
-  }
+    let newRemoveWatchlist = watchList.filter((item) => {
+      return item.id !== movie.id;
+    });
+    localStorage.setItem("watchlist", JSON.stringify(newRemoveWatchlist));
+    setWatchlist(newRemoveWatchlist);
+    console.log(newRemoveWatchlist);
+  };
 
-  useEffect(()=>{
-    let data = localStorage.getItem('watchlist')
-    if(data){
-      setWatchlist(JSON.parse(data))
+  useEffect(() => {
+    let data = localStorage.getItem("watchlist");
+    if (data) {
+      setWatchlist(JSON.parse(data));
     }
-  },[])
+  }, []);
 
   return (
     <>
@@ -45,13 +43,27 @@ function App() {
             path="/"
             element={
               <>
-                <Banner /> <Home handleAddtoWatchlist={handleAddtoWatchlist} handleRemoveWatchlist={handleRemoveFromWatchlist} watchList={watchList}/>
+                <Banner />{" "}
+                <Home
+                  handleAddtoWatchlist={handleAddtoWatchlist}
+                  handleRemoveWatchlist={handleRemoveFromWatchlist}
+                  watchList={watchList}
+                />
               </>
             }
           />
           {/* <Route path="/movies" element={<Movies />} /> */}
 
-          <Route path="/watchlist" element={<Watchlist watchList={watchList} setWatchlist={setWatchlist} handleRemoveFromWatchlist={handleRemoveFromWatchlist}/>} />
+          <Route
+            path="/watchlist"
+            element={
+              <Watchlist
+                watchList={watchList}
+                setWatchlist={setWatchlist}
+                handleRemoveFromWatchlist={handleRemoveFromWatchlist}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
